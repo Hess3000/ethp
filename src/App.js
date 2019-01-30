c9cf6d15c4a490d76be7b53849894012c5deded7
SDLondon <sjdlondon@gmail.com>
2019-01-30 21:05:46 +0000

    add footer

--- src/App.js
+++ src/App.js
@@ -23,19 +23,28 @@ const style = {
   }
 }
 
+const bodyCol = {
+  
+}
+
 class App extends Component {
   render() {
     return (
-      <Container fluid> 
+      <Container style={bodyCol} fluid> 
         <Header as='h1' textAlign='center' style={style.h1}>
           Cost of storing data on the Ethereum blockchain
         </Header>
-        <Segment basic>
-          Storing data on the Ethereum blockchain is expensive. It's best to store a hash of the data you wish to store. However, if you really need to store data on the Ethereum blockchain or you're just curious, here are the current prices for doing so. The cost of storing data changes all the time due to market forces, to get the latest conditions refresh the page. 
-        </Segment>
+        
+        
 
         <EthPrice>
         </EthPrice>
+
+        <Container>
+          <Segment>
+            Storing data on the Ethereum blockchain is expensive. It's best to store a hash of the data you wish to store. However, if you really need to store data on the Ethereum blockchain or you're just curious, here are the current prices for doing so. The cost of storing data changes all the time due to market forces, to get the latest conditions refresh the page. 
+          </Segment>
+        </Container>
 
         <Footer></Footer>
       </Container>
